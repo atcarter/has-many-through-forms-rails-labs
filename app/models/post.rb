4ebb6f264b44b06d13e95f8830f3433784b4b169
Antonio <atcarter005@gmail.com>
2021-07-27 02:56:22 -0500

    commit tree b57fd028f109241a3906cabef0bbdfa6fefbbde2

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes["name"] == ""}
 
+  #need to modify the default categories_attributes method so that we don't duplicate categories
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      # if category.name != ""
+      self.categories << category unless category.name.empty?
+      # end
+    end
+  end
 
 end
